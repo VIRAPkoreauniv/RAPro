db6fd30aa62e075f7a99a73278ab70da75b66350
차수지 <77491430+innerstella@users.noreply.github.com>
2024-09-01 15:49:14 +0900

    [feat] ESLint (#5)
    * feat: styled-components 설치
    
    * feat: 색상 토큰 설정
    
    * feat: Pretendard 웹폰트 연결
    
    * feat: html title 수정
    
    * feat: install project
    
    * feat: import order 설정

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+    :root{
+        --primiary: #444F79;
+        --secondary: #98AEFF;
+        --black: #333333;
+        --white: #FFFFFF;
+        --background: #F5F5F5;
+        --gray-middle:#787878;
+        --gray-light: #DFDFDF;
+        --gray-bright: #EDEDED;
+        --caution: #CD0000;
+    }
+`
+
+export default GlobalStyles
