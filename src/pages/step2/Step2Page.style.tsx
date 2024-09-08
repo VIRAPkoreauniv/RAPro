635cd7ba8089a0d87daa0c3a52038013feda1912
innerstella <suji0509@naver.com>
2024-09-08 17:09:48 +0900

    feat: step2 제목

---
+++ src/pages/step2/Step2Page.style.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 4.5rem;
+  overflow-y: scroll;
+
+  h1 {
+    color: #000;
+    font-size: 2.4rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+
+  p {
+    color: #000;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+`
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
--- src/pages/step2/index.tsx
+++ src/pages/step2/index.tsx
@@ -1,9 +1,26 @@
+import { useNavigate } from 'react-router-dom'
+import RectangleButton from '../../components/rectangle-button'
 import Layout from '../../layouts'
+import * as S from './Step2Page.style'
 
 export default function Step2Page() {
+  const navigate = useNavigate()
+
   return (
     <Layout>
-      <p>step2!</p>
+      <S.Wrapper>
+        <h1>STEP 2 : Input Site Data</h1>
+        <p>Please insert site data categorized into Source-Pathway-Receptor.</p>
+        <S.ButtonWrapper>
+          <RectangleButton
+            isActive={false}
+            size="medium"
+            onClick={() => navigate('/step/3')}
+          >
+            Next
+          </RectangleButton>
+        </S.ButtonWrapper>
+      </S.Wrapper>
     </Layout>
   )
 }
