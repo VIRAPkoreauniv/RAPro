97109bc1f204f971d4aabb8f5c6339295f7fe5ed
innerstella <suji0509@naver.com>
2024-09-11 18:29:52 +0900

    feat: NC_Risk 계산 식 (1,2,4,15)

--- src/pages/step3/index.tsx
+++ src/pages/step3/index.tsx
@@ -1,10 +1,14 @@
 import useComputeC from '../../utils/useComputeC'
+import useComputeNC from '../../utils/useComputeNC'
 
 export default function Step3Page() {
   const { C_Risk } = useComputeC()
+  const { NC_Risk } = useComputeNC()
+
   return (
     <>
       <h1>C Risk : {C_Risk}</h1>
+      <h1>NC Risk : {NC_Risk}</h1>
     </>
   )
 }
