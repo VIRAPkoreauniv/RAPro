1c8e57f410ad574f43ee9b6329dfb96787ec2ed9
innerstella <suji0509@naver.com>
2024-09-22 23:35:51 +0900

    feat: react query 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import AppRouter from "./Router"
+import { QueryClientProvider, useQueryClient } from '@tanstack/react-query'
+import AppRouter from './Router'
+
+const queryClient = useQueryClient()
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
       <AppRouter />
+    </QueryClientProvider>
   )
 }
 
 export default App
-
