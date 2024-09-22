12889e2a40f81135393c2cc0be6a0342ed2d81ca
innerstella <suji0509@naver.com>
2024-09-22 23:37:27 +0900

    feat: react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClientProvider, useQueryClient } from '@tanstack/react-query'
 import AppRouter from './Router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = useQueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <AppRouter />
     </QueryClientProvider>
   )
